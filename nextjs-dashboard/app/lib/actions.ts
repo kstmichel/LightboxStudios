f5e943a2ccd44d680be645998af0404305c241be
Katie St. Michel <katiestmichel@gmail.com>
2024-09-25 21:14:35 -0400

    added basic error handling

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -22,21 +22,27 @@ export async function createInvoice(formData: FormData) {
       status: formData.get('status'),
     });
     
+    // Forms with lots of fields can be converted to a plain object like this:
+    // const rawFormData = Object.fromEntries(formData.entries())
+
     const amountInCents = amount * 100;
 
     //TODO: install moment.js and handle these dates more robustly
     const date = new Date().toISOString().split('T')[0];
 
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    } catch(error) {
+        return  {
+            message: 'Database Error - Failed to create invoice'
+        }
+    };
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
-    
-    // Forms with lots of fields can be converted to a plain object like this:
-    // const rawFormData = Object.fromEntries(formData.entries())
 }
 
 
@@ -52,20 +58,38 @@ export async function updateInvoice(id: string, formData: FormData) {
  
   const amountInCents = amount * 100;
  
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+  try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+    `;
+    
  
+  } catch(error) {
+    return {
+        message: 'Database Error - Failed to update invoice'
+    }
+  };
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+  
 }
 
 export async function deleteInvoice(id: string) {
-    await sql`
-        DELETE FROM invoices
-        WHERE id = ${id}
-        `;
+    // throw new Error('Error deleting the invoice.');
+    
+    try {
+        await sql`
+            DELETE FROM invoices
+            WHERE id = ${id}
+            `;
+    } catch(error) {
+        return {
+            message: 'Database Error - Failed to delete invoice'
+        }
+    };
+
     revalidatePath('/dashboard/invoices');
 }
