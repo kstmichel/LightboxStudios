9298200104b706ec41d94ef7756a3b97f00fa1bc
Katie St. Michel <katiestmichel@gmail.com>
2024-10-15 01:47:41 -0400

    modified nav-links to resemble portfolio project navigation

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -3,33 +3,29 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
+import { roboto } from '@/app/ui/fonts';
 
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
-const links = [
-  { name: 'home', href: '/dashboard' },
-  { name: 'portfolio', href: '/dashboard/portfolio' },
-  { name: 'resume', href: '/dashboard/resume' },
-  {
-    name: 'invoices',
-    href: '/dashboard/invoices'
-  },
-  { name: 'customers', href: '/dashboard/customers' },
-];
+
 
 export default function NavLinks() {
   const pathname = usePathname();
 
+  const links = [
+    { name: 'about', href: '/dashboard' },
+    { name: 'portfolio', href: '/portfolio' },
+    { name: 'resume', href: '/resume' },
+  ];
+  
   return (
     <>
       {links.map((link) => {
-        const linkColor = pathname === link.href ? 'text-pink-600' : 'text-blue-400';
+        const linkColor = pathname === link.href || pathname && pathname.includes(link.href) ? 'text-orange-400' : 'text-blue-400';
 
         return (
           <Link
             key={link.name}
             href={link.href}
-            className={clsx(`flex h-[48px] grow items-center justify-center p-3 text-sm font-medium hover:text-blue-300 md:flex-none md:justify-start md:p-2 md:px-3 ${linkColor}`
+            className={clsx(`flex h-[48px] ${roboto.className} grow items-center justify-center p-3 text-sm font-medium hover:text-blue-300 md:flex-none md:justify-start md:p-2 md:px-3 ${linkColor}`
             )}
           >
             {/* <LinkIcon className="w-6" /> */}
