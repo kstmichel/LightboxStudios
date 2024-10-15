64963b66fa024cde42a880390120843f6c36d1bc
Katie St. Michel <katiestmichel@gmail.com>
2024-10-15 01:48:36 -0400

    UI styling updates, fonts & css

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,8 +1,18 @@
 import { Inter } from 'next/font/google';
-import { Lusitana } from 'next/font/google';
+import { Lusitana, Roboto, Roboto_Condensed } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Lusitana(
+    {
+        weight: ['400', '700'], 
+        subsets: ['latin']
+    });
+export const roboto = Roboto(
+    {
+        weight: ['400', '700'], 
+        subsets: ['latin']
+    });
+export const robotoCondensed = Roboto_Condensed(
     {
         weight: ['400', '700'], 
         subsets: ['latin']
