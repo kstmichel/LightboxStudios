11a02ad62b9c94afe8db63dbf6bf7faa3a6b4012
Katie St. Michel <katiestmichel@gmail.com>
2024-11-21 15:50:10 -0500

    added client-side validation for Description field being more than 255 characters.

--- nextjs-dashboard/app/lib/validation.ts
+++ nextjs-dashboard/app/lib/validation.ts
@@ -17,6 +17,7 @@ export const validateClientFields = (fields: FormFields) => {
     const errors: ValidationErrors = {};
     if (!fields.title) errors.title = 'Title is required';
     if (!fields.description) errors.description = 'Description is required';
+    if (fields.description.toString().length >= 255) errors.description = "Description needs to be less than 255 characters";
     if (!fields.image_url) errors.image_url = 'Image URL is required';
     if (!fields.alt) errors.alt = 'Image description is required';
     if (!fields.type) errors.type = 'Category is required';
