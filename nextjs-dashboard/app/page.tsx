5bdc0e1251e698148fdd9900b92026132feb0bd5
Katie St. Michel <katiestmichel@gmail.com>
2024-11-21 11:59:29 -0500

    adjusted css transitions on welcome screen

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -3,7 +3,6 @@ import React, {useState} from "react";
 import LightboxStudiosLogo from "@/app/ui/brand-logo";
 import { roboto } from "@/app/ui/fonts";
 import { Typography, Button } from "@mui/material";
-// import Image from 'next/image';
 
 export default function Page() {
   const [letThereBeLight, setLetThereBeLight] = useState(false);
@@ -21,12 +20,12 @@ export default function Page() {
             <Typography variant="h4"><strong>Welcome to <br />Lightbox Studios!</strong></Typography>
             <Typography variant="h5">A space just for you to bring your work into the light!</Typography>
         </div>
-          <div className="flex mt-8 centered flex-col grow text-center justify-center gap-4 w-full md:w-2/4">
+          <div className="flex mt-8 centered flex-col grow text-center justify-center gap-4 w-full md:w-1/4 md:min-w-64">
               <Button
                 href="/login"
                 variant="contained"
                 size="large"
-                className={`visible text-white transition-all 
+                className={`transition duration-700 ease-in-out text-white 
                     ${letThereBeLight ? 'bg-active hover:bg-active-dark' 
                       : 'bg-gray-800'}`}
                  >
@@ -37,9 +36,9 @@ export default function Page() {
                 href="/portfolio/web_development"
                 variant="outlined"
                 size="large"
-                className={`visible transition-all 
-                  ${letThereBeLight ? 'text-active border-active hover:border-active-dark' 
-                    : 'text-gray-800 border-gray-800'}`}
+                className={`transition duration-700 ease-in-out text-white
+                  ${letThereBeLight ? ' border-active hover:border-active-dark' 
+                    : ' border-gray-800'}`}
               >
                 Continue As Guest
               </Button>
